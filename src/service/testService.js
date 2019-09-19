149534375c2ea31289ad4864033185b74d249cec
nick <boy22200011@gmail.com>
2019-09-19 16:40:16 +0800

    2019-09-19

--- src/service/testService.js
+++ src/service/testService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default {
   get() {
     return axios.get(
-      'http://datacenter.taichung.gov.tw/swagger/OpenData/e409ced6-ed43-4cd7-b082-ed56351ceae4'
+      'https://datacenter.taichung.gov.tw/swagger/OpenData/e409ced6-ed43-4cd7-b082-ed56351ceae4'
     )
   }
 }
